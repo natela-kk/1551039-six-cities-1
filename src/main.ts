3013c5fd1bd23e478b6f353fe03a933a672a5ee3
Natela <n.kerd@mail.ru>
2022-05-10 19:30:26 +0300

    4.1

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ import LoggerService from './common/logger/logger.service.js';
 import { Component } from './types/component.types.js';
 import { ConfigInterface } from './common/config/config.interface.js';
 import Application from './app/application.js';
+import DatabaseService from './common/database-client/database.servise.js';
+import { DatabaseInterface } from './common/database-client/database.interface.js';
+import UserService from './../modules/user/user.service.js';
+import { UserServiceInterface } from './../modules/user/user-service.interface.js';
+import { UserEntity, UserModel } from '../modules/user/user.entity.js';
+import { ModelType} from '@typegoose/typegoose/lib/types.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService);
+applicationContainer.bind<ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -2,4 +2,7 @@ export const Component = {
   Application: Symbol.for('Application'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseInterface: Symbol.for('DatabaseInteface'),
+  UserServiceInterface: Symbol.for('UserServiceInterface'),
+  UserModel: Symbol.for('UserModel')
 } as const;

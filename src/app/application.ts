aa8037226997854fded1fc533e7e9aaedf2663dd
Natela <n.kerd@mail.ru>
2022-05-29 17:30:03 +0300

    5.2

--- src/app/application.ts
+++ src/app/application.ts
@@ -5,14 +5,19 @@ import { ConfigInterface } from '../common/config/config.interface';
 import { Component } from '../types/component.types.js';
 import { getURI } from '../utils/db.js';
 import { DatabaseInterface } from '../common/database-client/database.interface';
+import express, { Express } from 'express';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
-  ) {}
+  ) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization...');
@@ -27,5 +32,8 @@ export default class Application {
     );
     this.logger.info(uri);
     await this.databaseClient.connect(uri);
+
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost2:${this.config.get('PORT')}`);
   }
 }
